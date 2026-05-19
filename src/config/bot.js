077f7a7e2c9ae3ec04f262ac99cdef1f7e6537e2
jmhildawa-eng <jmhildawa@gmail.com>
2026-05-19 14:35:14 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -556,7 +556,7 @@ const client = new Client({
   ]
 });
 
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.DISCORD_TOKEN;
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
